4222df2e151a07e2d80ed56d2f585bfbd8a15c75
jeff.wang <jeff.wang@meetsocial.cn>
2019-12-25 19:07:23 +0800

    add yml

--- src/main/java/cn/demo/config/RabbitConfig.java
+++ src/main/java/cn/demo/config/RabbitConfig.java
@@ -12,69 +12,56 @@ import cn.demo.constant.MessageConstant;
 
 @Configuration
 public class RabbitConfig {
-
-	/********************* 测试交换机 *****************/
-
-	@Bean
-	public DirectExchange directExchange() {
-		return new DirectExchange(MessageConstant.ExchangeName.DIRECT_EXCHANGE, true, false);
-	}
-
-//	@Bean
-//	public FanoutExchange fanoutExchange() {
-//		return new FanoutExchange(MessageConstant.ExchangeName.FANOUT_EXCHANGE, true, false);
-//	}
-//
-//	@Bean
-//	public TopicExchange topicExchange() {
-//		return new TopicExchange(MessageConstant.ExchangeName.TOPIC_EXCHANGE, true, false);
-//	}
-
-	/**
-	 * 死信队列跟交换机类型没有关系 不一定为directExchange 不影响该类型交换机的特性.
-	 */
-	@Bean
-	public DirectExchange deadLetterExchange() {
-		return new DirectExchange(MessageConstant.ExchangeName.DELAY_EXCHANGE, true, false);
-	}
-
+	
+	
 	/********************* 测试队列 *****************/
 
+	/**
+	 * 定义转发到死信队列的队列
+	 */
+//	@Bean
+//	Queue directQueue() {
+//		// 消费失败的信息加入死信
+//		Queue directQueue = QueueBuilder.durable(MessageConstant.Queue.DIRECT_QUEUE)
+//				.deadLetterExchange(MessageConstant.Exchange.DLX_EXCHANGE)
+//				.deadLetterRoutingKey(MessageConstant.Route.DLX_ROUTE)
+//				.build();
+//		return directQueue;
+//	}
+	
 //	@Bean
 //	public Queue fanoutQueue() {
-//		Queue fanoutQueue = QueueBuilder.durable(MessageConstant.QueueName.FANOUT_QUEUE).build();
+//		Queue fanoutQueue = QueueBuilder.durable(MessageConstant.Queue.FANOUT_QUEUE).build();
 //		return fanoutQueue;
 //	}
 //
 //	@Bean
 //	public Queue topicQueue() {
-//		Queue topicQueue = QueueBuilder.durable(MessageConstant.QueueName.TOPIC_QUEUE).build();
+//		Queue topicQueue = QueueBuilder.durable(MessageConstant.Queue.TOPIC_QUEUE).build();
 //		return topicQueue;
 //	}
 
-	/**
-	 * 声明一个死信队列. x-dead-letter-exchange 对应 死信交换机 x-dead-letter-routing-key 对应死信路由建
-	 */
-	@Bean
-	Queue deadLetterQueue() {
-		return QueueBuilder.durable(MessageConstant.QueueName.DELAY_QUEUE)
-				.deadLetterExchange(MessageConstant.ExchangeName.DELAY_EXCHANGE) // DLX
-				.deadLetterRoutingKey(MessageConstant.RouteName.DELAY_ROUTE) // dead letter携带的routing key
-				.build();
-	}
 
-	/**
-	 * 定义死信队列转发队列.
-	 */
-	@Bean
-	public Queue directQueue() {
-		// 消费失败的信息加入死信
-		Queue directQueue = QueueBuilder.durable(MessageConstant.QueueName.DIRECT_QUEUE)
-				.deadLetterExchange(MessageConstant.ExchangeName.DELAY_EXCHANGE)
-				.deadLetterRoutingKey(MessageConstant.RouteName.DELAY_ROUTE)
-				.build();
-		return directQueue;
-	}
+	
+	
+	/********************* 测试交换机 *****************/
+	
+//	@Bean
+//	DirectExchange directExchange() {
+//		return new DirectExchange(MessageConstant.Exchange.DIRECT_EXCHANGE, true, false);
+//	}
+
+//	@Bean
+//	public FanoutExchange fanoutExchange() {
+//		return new FanoutExchange(MessageConstant.Exchange.FANOUT_EXCHANGE, true, false);
+//	}
+//
+//	@Bean
+//	public TopicExchange topicExchange() {
+//		return new TopicExchange(MessageConstant.Exchange.TOPIC_EXCHANGE, true, false);
+//	}
+
+
 
 	// 广播跟route-key 无关
 //	@Bean
@@ -87,9 +74,29 @@ public class RabbitConfig {
 //		return BindingBuilder.bind(topicQueue()).to(topicExchange()).with(MessageConstant.RouteName.TOPIC_ROUTE);
 //	}
 
+//	@Bean
+//	Binding directBinding() {
+//		return BindingBuilder.bind(directQueue()).to(directExchange()).with(MessageConstant.Route.DIRECT_ROUTE);
+//	}
+	
+	
+	/**
+	 * 声明一个死信队列
+	 */
 	@Bean
-	public Binding directBinding() {
-		return BindingBuilder.bind(directQueue()).to(directExchange()).with(MessageConstant.RouteName.DIRECT_ROUTE);
+	Queue deadLetterQueue() {
+		return QueueBuilder.durable(MessageConstant.Queue.DLX_QUEUE)
+				.deadLetterExchange(MessageConstant.Exchange.DLX_EXCHANGE)
+				.deadLetterRoutingKey(MessageConstant.Route.DLX_ROUTE)
+				.build();
+	}
+	
+	/**
+	 * 死信队列跟交换机类型没有关系 不一定为directExchange 不影响该类型交换机的特性.
+	 */
+	@Bean
+	DirectExchange deadLetterExchange() {
+		return new DirectExchange(MessageConstant.Exchange.DLX_EXCHANGE, true, false);
 	}
 
 	/**
@@ -98,6 +105,24 @@ public class RabbitConfig {
 	@Bean
 	Binding dlxBinding() {
 		return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange())
-				.with(MessageConstant.RouteName.DELAY_ROUTE);
+				.with(MessageConstant.Route.DLX_ROUTE);
 	}
+	
+	
+	/**
+	 * 可以配置MessageRecoverer对异常消息进行处理，此处理会在listener.retry次数尝试完并还是抛出异常的情况下才会调用，默认有两个实现：
+	 * RepublishMessageRecoverer：将消息重新发送到指定队列，需手动配置，如：
+	 * RejectAndDontRequeueRecoverer：如果不手动配置MessageRecoverer，会默认使用这个，实现仅仅是将异常打印抛出
+	 * @param rabbitTemplate
+	 * @return
+	 */
+//	@Bean
+//	public MessageRecoverer messageRecoverer(@Qualifier("rabbitTemplate") RabbitTemplate rabbitTemplate){
+//	    return new RepublishMessageRecoverer(rabbitTemplate,MessageConstant.Exchange.DLX_EXCHANGE , MessageConstant.Route.DLX_ROUTE);
+//	}
+//	
+//	@Bean
+//	public  RejectAndDontRequeueRecoverer rejectRecover()  {
+//		return new RejectAndDontRequeueRecoverer();
+//	};
 }
--- src/main/java/cn/demo/consumer/MessageConsumer.java
+++ src/main/java/cn/demo/consumer/MessageConsumer.java
@@ -1,8 +1,14 @@
 package cn.demo.consumer;
 
+import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Argument;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.rabbitmq.client.Channel;
@@ -12,25 +18,22 @@ import cn.demo.entity.MessageTemplate;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * bindings = @QueueBinding(
-        value = @Queue(name = MessageConstant.QueueName.DIRECT_QUEUE, durable = Exchange.TRUE,
-        autoDelete = MessageConstant.QueueName.DIRECT_QUEUE,
-        		arguments = {@Argument(name = "x-dead-letter-exchange", value = MessageConstant.ExchangeName.DELAY_EXCHANGE),
-        			@Argument(name = "x-dead-letter-routing-key", value = MessageConstant.RouteName.DELAY_ROUTE) }
-        ),
-        exchange = @Exchange(name = MessageConstant.ExchangeName.DIRECT_EXCHANGE,
-        type = ExchangeTypes.DIRECT,ignoreDeclarationExceptions = "true"),
-        key = {MessageConstant.RouteName.DIRECT_ROUTE})
+ * 
  * @author User
  *
  */
 @Slf4j
 @Component
-@RabbitListener(queues = MessageConstant.QueueName.DIRECT_QUEUE)
+@RabbitListener(bindings = @QueueBinding(
+		exchange = @Exchange(value = MessageConstant.Exchange.DIRECT_EXCHANGE, type = ExchangeTypes.DIRECT, durable = Exchange.TRUE, autoDelete = Exchange.FALSE),
+value = @Queue(value = MessageConstant.Queue.DIRECT_QUEUE, durable = "true", autoDelete = "false", arguments = {
+		@Argument(name = "x-dead-letter-exchange", value = MessageConstant.Exchange.DLX_EXCHANGE),
+		@Argument(name = "x-dead-letter-routing-key", value = MessageConstant.Route.DLX_ROUTE) }), 
+key = MessageConstant.Route.DIRECT_ROUTE))
 public class MessageConsumer {
-	
+
 	@RabbitHandler
-	public void handleMessage(byte[] payload, Message message, Channel channel) {
+	public void handleMessage(@Payload byte[] payload, Message message, Channel channel) {
 		Long deliveryTag = message.getMessageProperties().getDeliveryTag();
 		String data = new String(payload);
 		log.info("handleMessage:::payload={}, properties={}", data, message.getMessageProperties());
@@ -38,11 +41,20 @@ public class MessageConsumer {
 			channel.basicAck(deliveryTag, false);
 		} catch (Exception e) {
 			log.error(e.getMessage());
+			//可以通过给队列（Queue）绑定死信队列，使用nack反馈给mq，会将消息转发到死信队列里面，此种方式需要自己在消费消息的方法内部将异常处理掉
+			//消息确认时使用nack，并且requeue参数传false
+//			try {
+//				channel.basicNack(deliveryTag, false, false);
+//			} catch (IOException e1) {
+//				e1.printStackTrace();
+//			}
 		}
 	}
-	
+
 	@RabbitHandler
-	public void handleMessage(String data, Message message, Channel channel) {
+	public void handleMessage(@Payload String data, Message message, Channel channel){
+		int dd = 1/0;
+		System.out.print(dd);
 		Long deliveryTag = message.getMessageProperties().getDeliveryTag();
 		MessageTemplate template = MessageTemplate.builder().content(data).build();
 		log.info("handleMessage:::data={}, properties={}", template, message.getMessageProperties());
